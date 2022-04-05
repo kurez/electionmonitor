584f288fcc7140a594a225694ec15a8f3da800a5
kurez <william.kurez@gmail.com>
2022-04-05 05:08:45 +0300

    Logs page has been implemented

--- resources/assets/js/views/pages/Logs.vue
+++ resources/assets/js/views/pages/Logs.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+         <br>
+        <div class="row page-titles">
+            <div class="col-md-12 col-8 align-self-center">
+                <!-- <h3 class="text-themecolor m-b-0 m-t-0">User</h3> -->
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><router-link to="/electionmonitor/dashboard">Dashboard</router-link></li>
+                    <li class="breadcrumb-item active">Logs</li>
+                </ol>
+            </div>
+        </div>
     <v-data-table
         :headers="headers"
         :items="desserts"
